8cebfc704b825dd4e3ab8f65c55b81e22a229276
alecchangod <alec3d02@gmail.com>
2022-12-05 18:22:09 +0800

    vangogh: add config for xtended

---
+++ xtended_vangogh.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Inherit from vangogh device
+$(call inherit-product, device/xiaomi/vangogh/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := xtended_vangogh
+PRODUCT_DEVICE := vangogh
+BOARD_VENDOR := Xiaomi
+TARGET_VENDOR := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := M2002J9E
+
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Xtended Stuffs
+XTENDED_BUILD_MAINTAINER := alecchangod
+XTENDED_BUILD_TYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_BLUR := true
+XTENDED_BUILD_DONATE_URL := https://www.paypal.com/paypalme/Alec174
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vangogh-user 11 RKQ1.200826.002 V12.1.7.0.RJVCNXM release-keys"
+
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TD1A.220804.031/9071314:user/release-keys
